20a9d66e88073e236158458f44f813de2c3c6405
李昱岩 <794339312@qq.com>
2018-09-14 22:54:05 +0800

    init

---
+++ RMReport/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Data;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Collections;
+using ADODB;
+
+namespace RMReport
+{
+    public partial class Form1 : Form
+    {
+        //创建控件; hwnd为控件句柄，大于0为嵌入报表
+        [DllImport("MyReportMachine.dll")]
+        private static extern void MCreate(int hwnd);
+
+        //关闭控件并释放
+        [DllImport("MyReportMachine.dll")]
+        private static extern void MClose();
+
+        //清除所有变量与数据对象
+        [DllImport("MyReportMachine.dll")]
+        private static extern void MClearAll();
+
+        //将变量传入报表
+        //   objName:变量名称;
+        //   objValue:变量值(可以是字符串,数值,日期,是否等类型)
+        [DllImport("MyReportMachine.dll")]
+        private static extern void MAddV(object objName, object objValue);
+
+        //动态将数据集传入报表
+        //   objName:数据集名称;
+        //   objValue:Recordset数据集
+        [DllImport("MyReportMachine.dll")]
+        private static extern void MAddData(object objName, ADODB.Recordset rsData);
+
+        //控件中有数据源指定数据源对应关系(最多10个数据源）(不超过10个数据源时，请使用这个方法)
+        //   intNumber:数据源编号(0-9)共12个数据源
+        //   objValue:Recordset数据集
+        //   objName:数据集别名
+        [DllImport("MyReportMachine.dll")]
+        private static extern void MSetData(int intNumber, ADODB.Recordset rsData, object objName);
+
+        //报表打印
+        //   intKind: =0:报表预览；=1:打印报表；=2:报表设计；=3:报表准备；=4:显示准备的报表；
+        //   intShowDialog: =0:隐藏打印对话框, <>0:显示打印对话框
+        //   intProgress: =0:隐藏报表加载进度条, <>0:显示报表加载进度条
+        //   objFileName:报表文件路径
+        //   objPrinter:打印名称,="":默认打印
+        [DllImport("MyReportMachine.dll")]
+        private static extern void MPrintReport(int intKind, int intShowDialog, int intProgress, object objFileName, object objPrinter);
+
+        //--------------------------------------------------------以下是报表附属属性，可以不用调用
+        //返回报表页数
+        [DllImport("MyReportMachine.dll")]
+        private static extern int MReportCount();
+
+        //设报表预览按钮显示状态
+        //   intZoom：缩放按钮
+        //   intLoad：导出按钮
+        //   intSave：保存按钮
+        //   intPrint：打印按钮
+        //   intPageSetup：报表页面设置按钮
+        //   intExit：退出报表预览按钮
+        //   intSaveToXls：导出到Execl按钮
+        //   intExport：导出按钮
+        //   intNavigator：导航按钮
+        [DllImport("MyReportMachine.dll")]
+        private static extern void MPreviewButtons(int intZoom, int intLoad, int intSave, int intPrint, int intPageSetup, int intExit, int intSaveToXls, int intExport, int intNavigator);
+        //设置报表语言(默认中文)
+        [DllImport("MyReportMachine.dll")]
+        private static extern void MLanguage(object strFileName);
+
+        //设置预览模式；0:模式预览  1:嵌入预览
+        [DllImport("MyReportMachine.dll")]
+        private static extern void MSetPreview(int intKind);
+
+        //当前目录
+        string mainPath;
+        //数据库文件
+        string dbFile;
+        //查询数据
+        Hashtable dataDict;
+        //报表模板
+        string rmf;
+        //打印动作
+        int action;
+
+        public Form1(string rmf, string dbFile, Hashtable dataDict, int action)
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            base.SetVisibleCore(false);
+            this.dbFile = dbFile;
+            this.rmf = rmf;
+            this.dataDict = dataDict;
+            this.action = action;
+        }
+
+        //DataTable转RecordSet
+        public Recordset DsToRs(DataTable table)
+        {
+
+            Recordset rs = new RecordsetClass();
+            System.Array ArrA = System.Array.CreateInstance(typeof(string), table.Columns.Count);
+
+            foreach (DataColumn dc in table.Columns)
+            {
+                ArrA.SetValue(dc.ColumnName, dc.Ordinal);
+                rs.Fields._Append(dc.ColumnName, GetDataType(dc.DataType), -1, FieldAttributeEnum.adFldIsNullable);
+
+            }
+            rs.Open(Missing.Value, Missing.Value, CursorTypeEnum.adOpenUnspecified, LockTypeEnum.adLockUnspecified, -1);
+            //rs.Open(null,null, CursorTypeEnum.adOpenDynamic, LockTypeEnum.adLockOptimistic, 0);
+            // int i = 0;
+            foreach (DataRow dr in table.Rows)
+            {
+
+                rs.AddNew(Missing.Value, Missing.Value); //object o;
+                                                         // rs.AddNew(rs.Fields.,dr.ItemArray);
+
+                //rs.AddNew(ArrA, dr.ItemArray);
+                for (int i = 0; i < table.Columns.Count; i++)
+                {
+                    rs.Fields[i].Value = dr[i];
+                }
+            }
+            return rs;
+        }
+
+        //转类型
+        public static DataTypeEnum GetDataType(Type dataType)
+        {
+            switch (dataType.ToString())
+            {
+                case "System.Boolean": return DataTypeEnum.adBoolean;
+
+                case "System.Char": return DataTypeEnum.adChar;
+                case "System.DateTime": return DataTypeEnum.adDBTimeStamp;// .adDate;
+                case "System.Decimal": return DataTypeEnum.adDecimal;// .adNumeric;
+                case "System.Double": return DataTypeEnum.adDouble;
+                case "System.Int16": return DataTypeEnum.adSmallInt;
+                case "System.Int32": return DataTypeEnum.adInteger;
+                case "System.Int64": return DataTypeEnum.adBigInt;
+                case "System.Single": return DataTypeEnum.adSingle;
+                case "System.String": return DataTypeEnum.adVarWChar;
+                case "System.Byte[]": return DataTypeEnum.adVarBinary;
+                case "System.Object": return DataTypeEnum.adVariant;
+                case "System.Guid": return DataTypeEnum.adGUID;
+
+                case "System.Byte": return DataTypeEnum.adUnsignedTinyInt;
+                case "System.SByte": return DataTypeEnum.adTinyInt;
+                case "System.UInt16": return DataTypeEnum.adUnsignedSmallInt;
+                case "System.UInt32": return DataTypeEnum.adUnsignedInt;
+                case "System.UInt64": return DataTypeEnum.adUnsignedBigInt;
+
+
+                default: throw new Exception("没有对应的数据类型");
+
+            }
+        }
+
+        ///<summary>
+        /// 执行SQL返回数据; StrSql:查询语句; OleDbCon:数据库连接; T_Data:数据表
+        ///</summary>
+        public void My_OpenSql(string StrSql, System.Data.OleDb.OleDbConnection OleDbCon, DataTable T_Data)
+        {
+            System.Data.OleDb.OleDbDataAdapter DbCom;
+            DbCom = new System.Data.OleDb.OleDbDataAdapter();
+            if (OleDbCon.State != ConnectionState.Open)
+            {
+                OleDbCon.Open();
+            }
+            DbCom.SelectCommand = new System.Data.OleDb.OleDbCommand(StrSql, OleDbCon);
+            T_Data.Clear();
+            T_Data.Columns.Clear();
+            DbCom.Fill(T_Data);
+            OleDbCon.Close();
+            DbCom.Dispose();
+        }
+
+        //加载
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mainPath = System.IO.Directory.GetCurrentDirectory() + '\\';
+            this.OleDbCon.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + mainPath + this.dbFile + "; Persist Security Info=False";
+            PrintReport(this.action);
+            Environment.Exit(0);
+        }
+
+        //报表打印
+        private void PrintReport(int IntKind)
+        {
+            string StrReport, StrSql;
+            DataTable DtTemp;
+            StrReport = Application.StartupPath + @"\rmf\" + this.rmf;
+
+            MCreate(0); //报表创建 ； Hwnd可以为0 ； ***必须有
+            MClearAll(); //清除所有数据源及变量
+                         //oType.InvokeMember("MAddSn( "", "" ); //加入注册码  参数1(StrName):注册名,参数2(StrSn):注册码
+                         //1、变量的设置方法
+                         //Fr.MAddV(StrVName as String ,Vvalue),参数说明:：1、参数1(StrVName)变量名称;2、参数2(Vvalue)变量值(可以是字符串,数值,日期,是否等类型)
+            MAddV("V1", "中国");
+            MAddV("V2", 1);
+            MAddV("V3", 0.87);
+            MAddV("V4", DateTime.Now);
+            MAddV("V5", false);
+            MAddV("V6", "sadfasd");
+
+            MAddV("MyV", DateTime.Now);
+
+            //2、数据源的设法方法
+            //动态加载数据源
+            //使用动态加载数据源时,请先清除数据源
+
+            //控件中有数据源指定数据源对应关系(最多10个数据源）(不超过10个数据源时，请使用这个方法)
+            //Rm.MSetData(MSetData(IntNumber As Long, RsData As Object, StrName as String)
+            //参数说明：
+            //1、参数1(IntNumber):数据源编号(0-9)共10个数据源
+            //2、参数2(RsData)是Recordset数据源;
+            //3、参数3(StrName)数据源别名
+            int i = 0;
+            foreach (DictionaryEntry d in this.dataDict)
+            {
+                StrSql = (string)d.Value;
+                DtTemp = new DataTable();
+                this.My_OpenSql(StrSql, OleDbCon, DtTemp);
+                MSetData(i++, this.DsToRs(DtTemp), (string)d.Key);
+            }
+
+            //报表打印
+            //MPrintReport(IntKind As Long, IntShowDialog As Long, IntProgress As Long, StrFileName as String, StrPrinter  as String)
+            //参数说明:
+            //1、参数1(IntKind): =0:报表预览；=1:打印报表；=2:报表设计；=3:报表准备；=4:显示准备的报表；
+            //2、参数2(IntShowDialog): =0:隐藏打印对话框, <>0:显示打印对话框
+            //3、参数3(IntProgress): =0:隐藏报表加载进度条, <>0:显示报表加载进度条
+            //4、参数4(StrFileName):报表名称
+            //5、参数5(StrPrinter):打印名称,="":默认打印
+
+            switch (IntKind)
+            {
+                case 1:
+                    {
+                        MPrintReport(1, 1, 0, StrReport, "");
+                    }
+                    break;
+                case 2:
+                    {
+                        MPrintReport(2, 1, 0, StrReport, "Adobe PDF");//"Adobe PDF") '指定打印名称:"Adobe PDF"
+                    }
+                    break;
+                default:
+                    {
+                        MPrintReport(0, 1, 0, StrReport, "");
+                    }
+                    break;
+            };
+
+            MClose(); //卸载 ；***必须有
+        }
+    }
+}
---
+++ RMReport/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace RMReport
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args) 
+        {
+            var arguments = CommandLineArgumentParser.Parse(args);
+            string dbFile = "balance.db";
+            string dbStr = "data:select * from t_balance;";
+            string rmf = "过称单(标准式).rmf";
+            int action = 2;
+            int argv = 4;
+            if (arguments.Has("-whosyourdaddy"))
+            {
+                argv = 4;
+            }
+            else
+            {
+                if (arguments.Has("-d"))
+                {
+                    dbFile = arguments.Get("-d").Next;
+                    argv++;
+                }
+                if (arguments.Has("-s"))
+                {
+                    dbStr = arguments.Get("-s").Next;
+                    argv++;
+                }
+                if (arguments.Has("-r"))
+                {
+                    rmf = arguments.Get("-r").Next;
+                    argv++;
+                }
+                if (arguments.Has("-a"))
+                {
+                    action = Convert.ToInt32(arguments.Get("-a").Next);
+                    argv++;
+                }
+            }
+            if(argv != 4)
+            {
+                MessageBox.Show("参数错误！", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);
+            }
+
+            Hashtable dataDict = new Hashtable();
+            // 分号分隔
+            string[] dbStrPair = dbStr.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string item in dbStrPair)
+            {
+                // 冒号分隔
+                string[] kv = item.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                dataDict.Add(kv[0], kv[1]);
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form f = new Form2(rmf, dbFile, dataDict, action);
+            Application.Run(f);
+        }
+    }
+}
